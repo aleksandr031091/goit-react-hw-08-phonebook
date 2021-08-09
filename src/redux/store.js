36801b7e945853d1eae36684e1501fc2f84ca92e
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-08-09 22:07:03 +0300

    start hw-08 add base file

---
+++ src/redux/store.js
@@ -0,0 +1,36 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: [""],
+};
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, rootReducer),
+  middleware,
+  //   devTools: process.env.NODE_ENV !== "production",
+});
+
+persistStore(store);
+
+export default store;
